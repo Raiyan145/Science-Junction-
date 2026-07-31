6e85fc9fb2e916ad8d470169953532e5e160cf11
Raiyan145 <raiyan143315@gmail.com>
2026-07-31 15:01:39 +0600

    Create home.js

---
+++ js/components/pages/home.js
@@ -0,0 +1,9 @@
+// js/pages/home.js
+import { initThemeToggle } from '../components/theme-toggle.js';
+import { initNavbar } from '../components/navbar.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initThemeToggle();
+    initNavbar();
+    // Phase 6 logic will be added here later (e.g., fetchTrendingArticles())
+});
